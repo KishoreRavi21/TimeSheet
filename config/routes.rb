bad5ebd8855a524dae26af9132b057ec6d41a54f
KishoreRavi21 <120175676+KishoreRavi21@users.noreply.github.com>
2022-12-09 16:47:33 +0530

    2nd
    form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # root to: "students#index"
+  root to: "students#index"
   resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
